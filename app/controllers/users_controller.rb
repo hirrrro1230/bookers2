afd6e54ec6bdc685d1605b7b45a36fe808941aae
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2022-09-03 13:10:26 +0000

    Fifth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @user = current_user
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#about"
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update]
+  post 'books/:id/edit' => 'books#edit'
   resources :users, only: [:show, :edit, :index]
 end
